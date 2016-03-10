3ca4ce9d982c3288789dbb0e810d27d4ac5091b3
Gilles <godiveau@gmail.com>
2016-03-10 19:24:19 +0100

    2.0 testing if pair ou impair

--- data/js/boom.js
+++ data/js/boom.js
@@ -12,30 +12,30 @@ $select = document.getElementById("PairImpair");
 total = 10000;
 totalbank = 100000;
 
-//selection paire impaire
+// Selection paire impair
 function selitem(){
     $choix = $select.selectedIndex;
     $valeur = $select.options[$choix].value;
 }
 
-//selection du chiffre sur lequel miser
+// Selection du chiffre sur lequel miser
 function selnumber(){
     $nchoix = $nselect.selectedIndex;
     $nvaleur = $nselect.options[$nchoix].value;
 }
 
-//selection d un nombre entre 0 et 36
+// Selection d un nombre entre 0 et 36
 function nombreroulette(){
     roulette = Math.floor(Math.random() * 37);
     $displayroulette.innerHTML = roulette
 }
 
-//recupere la somme a miser dans le input
+// Récupère la somme à miser dans le input
 function amiser(){
     somme = document.getElementById("somme").value;
 }
 
-//actualisation de l argent en cas de perte
+// Actualisation de l'argent en cas de perte
 function loose(){
     total = total - somme;
     totalbank = totalbank + (somme *1);
@@ -43,7 +43,7 @@ function loose(){
     gameover();
 }
 
-//actualisation de l argent quand la roulette tombe susr le nombre qu on a choisis
+// Actualisation de l'argent quand la roulette tombe susr le nombre qu'on a choisis
 function win(){
     total = total + (somme * 36);
     totalbank = totalbank - (somme * 36);
@@ -51,7 +51,7 @@ function win(){
     gameover();
 }
 
-//actualisation de l argent quand on a bien choisis paire ou impaire
+// Actualisation de l'argent quand on a bien choisis paire ou impair
 function littlewin(){
     total = total + (somme * 2);
     totalbank = totalbank - (somme * 2 );
@@ -59,7 +59,7 @@ function littlewin(){
     gameover();
 }
 
-//message gagner perdu en cas de bankroute
+// Message gagner perdu en cas de bankroute
 function gameover(){
     enbank();
     if(total <= 0){
@@ -69,14 +69,22 @@ function gameover(){
     }
 }
 
-//affiche notre total d argent sur la page html
+// Affiche notre total d'argent sur la page html
 function enbank(){
     $argent.innerHTML = total;
     $argentBank.innerHTML = totalbank;
 }
 
-//pour savoir si on gagne ou on pert
+// Pour savoir si une mise à été placée
+function testing() {
+        if($("select").val() === ''){
+        $displayresult.innerHTML = "Tentez pair ou impair quand même ! Lancez vous !"
+    }
+}
+
+//Pour savoir si on gagne ou on pert
 function resultat(){
+    testing()
     nombreroulette();
     amiser();
     if((roulette != $nvaleur) && ((roulette % 2) != $valeur) || (roulette === 0)){
