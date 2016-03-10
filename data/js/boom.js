c2d76c85f0b893509d57020aefa6296cc6ec2f37
Gilles <godiveau@gmail.com>
2016-03-10 21:54:24 +0100

    2.1 Modals sur banqueroute

--- data/js/boom.js
+++ data/js/boom.js
@@ -6,7 +6,7 @@ $argentBank = document.getElementById("argentBank");
 $bouton = document.getElementById("bouton");
 $displayresult = document.getElementById("displayresult");
 $displayroulette = document.getElementById("displayroulette");
-$nselect = document.getElementById("SelctedNumber");
+$nselect = document.getElementById("SelectedNumber");
 $select = document.getElementById("PairImpair");
 
 total = 10000;
@@ -24,7 +24,7 @@ function selnumber(){
     $nvaleur = $nselect.options[$nchoix].value;
 }
 
-// Selection d un nombre entre 0 et 36
+// Selection d'un nombre entre 0 et 36
 function nombreroulette(){
     roulette = Math.floor(Math.random() * 37);
     $displayroulette.innerHTML = roulette
@@ -43,7 +43,7 @@ function loose(){
     gameover();
 }
 
-// Actualisation de l'argent quand la roulette tombe susr le nombre qu'on a choisis
+// Actualisation de l'argent quand la roulette tombe sur le nombre choisi
 function win(){
     total = total + (somme * 36);
     totalbank = totalbank - (somme * 36);
@@ -62,12 +62,18 @@ function littlewin(){
 // Message gagner perdu en cas de bankroute
 function gameover(){
     enbank();
-    if(total <= 0){
-        $displayresult.innerHTML = "Vous avez tout perdu, même le slip ! f5 pour reperdre."
-    }else if (totalbank <= 0){
-        $displayresult.innerHTML = "Bravo vous avez fait sauter la banque ! je suis viré"
-    }
+        if(total <= 0){
+    $(document).ready(function(){
+        $("#modalrejouer").modal('show');
+    });
+        }else if (totalbank <= 0){
+    $(document).ready(function(){
+        $("#modalrejouergagne").modal('show');
+    });
+        }
 }
+
+
 
 // Affiche notre total d'argent sur la page html
 function enbank(){
@@ -75,16 +81,26 @@ function enbank(){
     $argentBank.innerHTML = totalbank;
 }
 
-// Pour savoir si une mise à été placée
-function testing() {
-        if($("select").val() === ''){
-        $displayresult.innerHTML = "Tentez pair ou impair quand même ! Lancez vous !"
+
+// Pour savoir si l'argent est misée
+function testingMise() {
+        if($("#somme").val() === ''){
+        $displayresult.innerHTML = "Vous oubliez de placer votre mise, rien ne vas plus !"
     }
 }
 
-//Pour savoir si on gagne ou on pert
+
+// Pour savoir si un select est choisi
+function testingSelect() {
+        if($("#PairImpair").val() === ''){
+        $displayresult.innerHTML = "Vous devriez choisir une couleur ou un numéro non ?"
+    }
+}
+
+// Lancer le jeux puis savoir si on gagne ou on pert
 function resultat(){
-    testing()
+    testingMise();
+    testingSelect();
     nombreroulette();
     amiser();
     if((roulette != $nvaleur) && ((roulette % 2) != $valeur) || (roulette === 0)){
